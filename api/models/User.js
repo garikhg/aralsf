6555e0bcb3c7a5017fc9c80d367b381691d6064a
garikhg <garik.28h@gmail.com>
2024-08-03 01:15:09 +0400

    Add user email field and routes for user operations
    Introduce an email field to the User model, ensuring uniqueness. Create API routes for adding and retrieving users, integrating the userRoutes module in the server.

--- api/models/User.js
+++ api/models/User.js
@@ -1,1 +1,1 @@
-const mongoose = require( 'mongoose' );const userSchema = new mongoose.Schema( {    name: {        type: String,        required: true    }} );const User = mongoose.model( 'User', userSchema );module.exports = User;
+const mongoose = require( 'mongoose' );const userSchema = new mongoose.Schema( {    name: {        type: String,        required: true    },    email: {        type: String,        required: false,        unique: true    }} );const User = mongoose.model( 'User', userSchema );module.exports = User;
