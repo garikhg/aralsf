cbdcb859f0ca0c506cb031aff46d1ef45f05d396
garikhg <garik.28h@gmail.com>
2024-08-03 03:31:00 +0400

    Refactor layout and add lint fix script
    Removed unused font import and class references in layout.tsx. Added a div wrapper to the Home component. Enhanced package.json with a new lint:fix script. Corrected tailwind.config.ts syntax.

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <>Admin Panel</>
+    <div>Admin Panel</div>
   );
 }
--- admin/tailwind.config.ts
+++ admin/tailwind.config.ts
@@ -8,9 +8,8 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {,
-    },
+    extend: {},
   },
-  plugins: [],
-    };
+  plugins: []
+};
 export default config;
