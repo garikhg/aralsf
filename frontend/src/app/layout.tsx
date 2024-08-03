8f21463068ab1fd35873a352823b8275795b79bf
garikhg <garik.28h@gmail.com>
2024-08-03 17:12:09 +0400

    Add new Brand Logo and Button components with header
    Introduced a new SVG brand logo and button component based on Radix UI. Updated layout to include a header featuring the new logo and a search button. Adjusted global styles to reflect design changes.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
+import {BrandLogo} from "@/_components/Brand";
+import NextLink from "next/link";
+import {Button} from "@/components/ui/button";
+import {Search} from "lucide-react";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -14,7 +18,29 @@ export default function RootLayout({children}: Readonly<{ children: React.ReactN
     return (
         <html lang="en" suppressHydrationWarning={true}>
         <body className={inter.className}>
-        {children}
+        <header className="bg-primary" role="main">
+            <div className="container flex justify-between items-center">
+                <div className="flex items-center mr-auto">
+                    <NextLink href="/">
+                        <BrandLogo/>
+                    </NextLink>
+                </div>
+                <div className="text-primary-foreground">Navigation</div>
+                <div>
+                    <Button variant="default" size="default"
+                            className="h-14 w-14 p-4 text-primary-foreground hover:text-primary-foreground/90">
+                        <Search size={24}/>
+                        <span className="sr-only">Search</span>
+                    </Button>
+                </div>
+            </div>
+        </header>
+        <main className="container" role="main">
+            {children}
+        </main>
+        <footer>
+            <div className="container">Footer</div>
+        </footer>
         </body>
         </html>
     );
