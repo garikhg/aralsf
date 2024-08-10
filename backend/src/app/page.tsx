e5ad3e3d3b340ef49fe484b26fad615a54eaabed
garikhg <garik.28h@gmail.com>
2024-08-10 19:01:29 +0400

    Add NextAuth integration and remove custom signup
    Replaced the custom signup logic with NextAuth for authentication, utilizing PrismaAdapter for database interactions. Updated dependencies in package-lock.json to include NextAuth and corresponding packages, and adjusted .gitignore accordingly.

--- backend/src/app/page.tsx
+++ backend/src/app/page.tsx
@@ -1,4 +1,15 @@
-export default async function Home() {
+"use client";
+
+import React from 'react';
+import {signOut, useSession} from "next-auth/react";
+
+export default function Home() {
+    const {data: session, status} = useSession();
+
+    console.log(status)
+    if (!session) {
+        return <p>You are not logged in.</p>;
+    }
     return (
        <div>
            Login Page
