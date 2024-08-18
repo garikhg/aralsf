467f4d0c399e6e9dbbafc8519cb80b458f2fa1ed
garikhg <garik.28h@gmail.com>
2024-08-18 21:49:06 +0400

    Refactor Dashboard function to remove async operations.
    The async operations within the Dashboard function have been commented out. This change simplifies the current version and will require further adjustments to reintroduce data fetching.

--- backend/src/app/(dashboard)/page.tsx
+++ backend/src/app/(dashboard)/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import prisma from '@lib/prisma';
 
-export default async function Dashboard() {
-  const posts = await prisma.post.findMany( {
-    where: { published: true },
-    orderBy: { createdAt: 'desc' }
-  } );
+export default function Dashboard() {
+  // const posts = await prisma.post.findMany( {
+  //   where: { published: true },
+  //   orderBy: { createdAt: 'desc' }
+  // } );
+  //
+  // console.log(posts)
 
   return (
     <div>
