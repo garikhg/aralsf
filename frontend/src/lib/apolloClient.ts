953514c449d008aadfebaf1267b0ec96bae23aaf
garikhg <garik.28h@gmail.com>
2024-08-19 22:12:04 +0400

    Refactor Apollo Client initialization and implement caching
    Revised Apollo Client setup to use memoization and improve performance. Added ApolloProvider in RootLayout for client state management. This ensures efficient rehydration and state management across SSR and client-side environments.

--- frontend/src/lib/apolloClient.ts
+++ frontend/src/lib/apolloClient.ts
@@ -1,12 +1,34 @@
+import { useMemo } from 'react';
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
-const useApolloClient = () => {
-  return new ApolloClient({
-    link: new HttpLink({
-      uri: process.env.NEXT_PUBLIC_WORDPRESS_URL, // Replace with your WordPress GraphQL endpoint
-    }),
-    cache: new InMemoryCache(),
-  });
-};
+let apolloClient: ApolloClient<any> | undefined;
 
-export default useApolloClient;
+function createApolloClient() {
+  return new ApolloClient( {
+    link: new HttpLink( { uri: process.env.NEXT_PUBLIC_WORDPRESS_URL } ),
+    cache: new InMemoryCache()
+  } );
+}
+
+export function initializeApollo(initialState: any = {}) {
+  const _apolloClient = apolloClient ?? createApolloClient();
+
+  // Hydrate the Apollo client state if initial state is provided
+  if (initialState) {
+    _apolloClient.cache.restore( initialState );
+  }
+
+  if (typeof window === 'undefined') {
+    return _apolloClient; // SSG and SSR
+  }
+  if (!apolloClient) {
+    apolloClient = _apolloClient; // Create the Apollo Client once in the client
+  }
+
+  return apolloClient;
+}
+
+export function useApollo(initialState: any = {}) {
+  const store = useMemo( () => initializeApollo( initialState ), [initialState] );
+  return store;
+}
