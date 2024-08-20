596dbcbee5f30a8a35587a5c157f29d472848b59
garikhg <garik.28h@gmail.com>
2024-08-20 23:16:42 +0400

    Delete product-category page and integrate dynamic fetching
    Removed the static product-category page and updated the `[slug]` page to dynamically fetch category data using Apollo Client. Additionally, enhanced the PageHeader component to support dynamic background images.

--- frontend/src/app/product-category/[slug]/page.tsx
+++ frontend/src/app/product-category/[slug]/page.tsx
@@ -1,42 +1,70 @@
-import React from 'react';
+'use client';
 
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
 import { PageHeader } from '@/components/page-header';
 import ProductsFilters from '@/components/products/products-filters';
 import ProductCard from '@/components/products/product-card';
+import { gql, useQuery } from '@apollo/client';
 
-const products = [
-  {
-    id: 1,
-    title: 'Alazani Valley Red Semi-Sweet Wine',
-    sku: 'DSC3948',
-    alco: '4.5%',
-    vol: '750ml',
-    imageSrc: '/images/demo/products/alazani-valley-r-DSC3948.jpg'
-  },
-  {
-    id: 2,
-    title: 'Khvantchkara Red Semi-Sweet Wine',
-    sku: 'DSC3944',
-    alco: '4.5%',
-    vol: '750ml',
-    imageSrc: '/images/demo/products/khvantchkara-DSC3944.jpg'
-  },
-  {
-    id: 3,
-    title: 'Kindzmarauli Red Semi-Sweet Wine',
-    sku: 'DSC3944',
-    alco: '4.5%',
-    vol: '750ml',
-    imageSrc: '/images/demo/products/kindzmarauli-DSC3950.jpg'
+const GET_CATEGORY_BY_SLUG = gql`
+    query GetCategoryBySlug($idType: AcfProductCatIdType = SLUG, $id1: ID!) {
+        acfProductCat(idType: $idType, id: $id1) {
+            id
+            name
+            slug
+            description
+            termTaxonomyId
+            acfProductCategoriesOptions {
+                acfThumbnail {
+                    node {
+                        sourceUrl
+                        altText
+                        slug
+                    }
+                }
+                acfHeroBanner {
+                    node {
+                        altText
+                        sourceUrl
+                    }
+                }
+            }
+        }
+    }
+`;
+
+const SingleCategory: React.FC = () => {
+  const { slug } = useParams();
+  const [category, setCategory] = useState<any>( null );
+  const [heroBanner, setHeroBanner] = useState<any>( null );
+
+  const { data, error } = useQuery( GET_CATEGORY_BY_SLUG, {
+    variables: { id1: slug },
+    skip: !slug
+  } );
+
+  useEffect( () => {
+    if (data?.acfProductCat) {
+      setCategory( data?.acfProductCat );
+
+      if (data?.acfProductCat?.acfProductCategoriesOptions?.acfHeroBanner?.node) {
+        setHeroBanner( data?.acfProductCat?.acfProductCategoriesOptions?.acfHeroBanner?.node );
+      }
+    }
+
+  }, [data] );
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
   }
-];
-
-export default async function SingleCategory() {
 
   return (
     <div>
       <PageHeader
-        title="Beer"
+        title={category?.name || 'Category'}
+        description={category?.description || ''}
+        backgroundImage={heroBanner?.sourceUrl || ''}
       />
 
       <main className="py-24" role="main">
@@ -65,7 +93,7 @@ export default async function SingleCategory() {
       </main>
     </div>
   );
-}
+};
 
 const Filters = () => {
   return (
@@ -74,3 +102,5 @@ const Filters = () => {
     </aside>
   );
 };
+
+export default SingleCategory;
