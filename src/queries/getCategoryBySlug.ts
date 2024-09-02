074b8d720fa35155809ac732fe41d3756e995f55
garikhg <garik.28h@gmail.com>
2024-09-02 21:13:23 +0400

    Add Queries

---
+++ src/queries/getCategoryBySlug.ts
@@ -0,0 +1,78 @@
+import { gql } from '@apollo/client';
+
+const productDetailsFragment = gql`
+    fragment ProductDetails on Product {
+        slug
+        status
+        title
+        featuredImage {
+            node {
+                sourceUrl
+            }
+        }
+        acfProductOptions {
+            brand
+            country
+            manufacturer
+            sku
+            color
+            grapeType
+            type
+            alcoholVolume
+            acfProductAttribute {
+                acfProductAttributeName
+                acfProductAttributeValue
+            }
+        }
+    }
+`;
+
+export const GET_CATEGORY_BY_SLUG = gql`
+    ${productDetailsFragment}
+    query GetCategoryBySlug(
+        $idType: AcfProductCatIdType = SLUG,
+        $id1: ID!
+        $productsFirst: Int = 6
+        $productsLast: String,
+    ) {
+        acfProductCat( idType: $idType, id: $id1) {
+            id
+            name
+            slug
+            description
+            termTaxonomyId
+            acfProductCategoriesOptions {
+                acfThumbnail {
+                    node {
+                        sourceUrl
+                        altText
+                        slug
+                    }
+                }
+                acfHeroBanner {
+                    node {
+                        altText
+                        sourceUrl
+                    }
+                }
+            }
+            products(
+                first: $productsFirst,
+                after: $productsLast,
+                where: {
+                    status: PUBLISH,
+                    orderby: {field: MENU_ORDER, order: ASC},
+                }
+            ) {
+                nodes {
+                    ...ProductDetails
+                }
+                pageInfo {
+                    hasNextPage
+                    hasPreviousPage
+                    endCursor
+                }
+            }
+        }
+    }
+`;
