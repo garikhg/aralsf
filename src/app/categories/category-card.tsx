c30b544e21547d5f6faee931df3efad0e19203ed
garikhg <garik.28h@gmail.com>
2024-09-28 01:42:05 +0400

    Refactor categories page and remove unused components
    Simplified the categories page by integrating server-side data fetching and updated the CategoryCard component. Removed the outdated CategoryCard and loading components that are no longer in use. This change improves data handling and ensures the UI stays responsive without unnecessary client-side loading states.

---
+++ src/app/categories/category-card.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import {Button} from "@/components/ui/button";
+
+
+const getCatLink = (name: string, slug: string, count: number) => (
+    <>
+        {count > 0 ? (
+            <Link href={`/product-category/${slug}`}
+                  className="inline-block leading-tight after:h-px after:block after:bg-white after:w-0 hover:after:w-full after:transition-all after:duration-200"
+            >
+                <span>See all products</span>
+            </Link>
+        ) : (
+            <Button
+                className="text-white px-0"
+                variant="link"
+                disabled={true}
+            >
+                <span>Not Available Products</span>
+            </Button>
+        )}
+    </>
+)
+
+const CategoryCard = ({data}: { data: any }) => {
+    const {name, description, slug, count} = data;
+    const {url: thumbnailUrl, alt} = data?.acf?.thumbnail;
+    const altName = !alt ? name : alt;
+
+    return (
+        <div className="relative overflow-hidden rounded-lg min-h-[468px]">
+            {thumbnailUrl &&
+                <div className="w-full h-full relative top-0 left-0 z-0">
+                    <span className="absolute w-full h-full top-0 left-0 bg-gradient-to-t from-black/70 to-black/10 z-[5]"></span>
+                    <Image
+                        src={thumbnailUrl}
+                        alt={altName}
+                        width={720}
+                        height={776}
+                        priority
+                        className="w-full h-full blank object-cover max-w-full z-0"
+                    />
+                </div>
+            }
+            <div className="absolute bottom-0 w-full text-primary-foreground p-8 mt-auto">
+                {name && <h3 className="block scroll-m-20 text-2xl font-semibold tracking-tight mb-3">{name}</h3>}
+                {description && <div className="min-h-20 pb-6"><p dangerouslySetInnerHTML={{__html: description}} className="leading-6"/></div>}
+                {getCatLink( name, slug, count )}
+            </div>
+        </div>
+    );
+};
+
+
+export default CategoryCard;
--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,72 +1,62 @@
-'use client';
+import React from 'react';
+import {Metadata} from "next";
+import {getPageCategory, getProductCategories} from "@/lib/wordpress";
+import CategoryCard from "@/app/categories/category-card";
+import Image from "next/image";
 
-import React, { Suspense, useEffect, useState } from 'react';
-import { CategoryCard } from '@/app/categories/CategoryCard';
-import { BrandPartnersCarousel } from '@/components/BrandPartners/BrandPartnersCarousel';
-import { gql, useQuery } from '@apollo/client';
-import { PageHeader } from '@/components/layouts/page-header';
-import Loading from '@/app/categories/loading';
-
-const Categories = () => {
-
-
-  const [categories, setCategories] = useState( [] );
-  const { data, loading, error } = useQuery( GET_CATEGORIES_QUERY );
-
-  useEffect( () => {
-    if (data?.acfProductCats?.nodes) {
-      setCategories( data.acfProductCats.nodes );
+export const generateMetadata = async (): Promise<Metadata> => {
+    const page = await getPageCategory();
+    return {
+        title: page[0]?.title?.rendered || 'Categories',
+        description: "Categories Description",
     }
-  }, [data] );
+}
 
-  if (loading) {
-    return <Loading />;
-  }
+const Categories: React.FC = async () => {
+    const categories = await getProductCategories();
+    const pageCategory = await getPageCategory();
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+    const pageTitle = pageCategory[0]?.title?.rendered || 'Categories';
+    // @ts-ignore
+    const featuredMedia = pageCategory[0]?._embedded?.['wp:featuredmedia'][0]?.source_url || null;
 
+    return (
+        <div className="min-h-screen">
+            {pageTitle && (
+                <div className="container relative">
+                    <div className="absolute top-1/2 left-0 right-0 -translate-y-1/2 z-20">
+                        <div className="px-6 lg:px-16 text-primary-foreground text-center">
+                            <h1 className="text-5xl font-bold">{pageTitle}</h1>
+                        </div>
+                    </div>
 
-  return (
-    <div>
-      <PageHeader title="Categories" />
-      <main role="main">
-        <div className="container py-8 lg:py-24">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {categories && categories.map( (category: any) => (
-              <CategoryCard
-                key={category.slug}
-                data={category}
-              />
-            ) )}
-          </div>
+                    {featuredMedia && (
+                        <div className="relative rounded-lg overflow-hidden">
+                            <span className="w-full h-full absolute top-0 left-0 bg-gradient-to-t from-black/60 to-transparent"></span>
+                            <Image
+                                src={featuredMedia}
+                                alt={pageTitle}
+                                width={1728}
+                                height={460}
+                                className="w-full max-w-full h-[460px] block object-cover rounded-lg"
+                            />
+                        </div>
+                    )}
+                </div>
+            )}
+            <div className="container py-6 sm:py-8 lg:py-16">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    {categories && categories.map( (category: any) => (
+                        <div key={category.slug} className="relative">
+                            <CategoryCard data={category}/>
+                        </div>
+                    ) )}
+                </div>
+            </div>
+
+            {/*<BrandPartnersCarousel/>*/}
         </div>
-        <BrandPartnersCarousel />
-      </main>
-    </div>
-  );
+    );
 };
-
-const GET_CATEGORIES_QUERY = gql`
-    query GetProductCategories($exclude1: [ID] = "12") {
-        acfProductCats(where: {exclude: $exclude1, order: ASC, orderby: TERM_ORDER}) {
-            nodes {
-                name
-                uri
-                slug
-                description
-                acfProductCategoriesOptions {
-                    acfThumbnail {
-                        node {
-                            sourceUrl
-                            altText
-                        }
-                    }
-                }
-            }
-        }
-    }
-`;
 
 export default Categories;
