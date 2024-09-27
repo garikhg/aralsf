eaa04e37ba369e6b71d7ee382df59712f0dce120
garikhg <garik.28h@gmail.com>
2024-09-28 01:45:36 +0400

    Remove ProductFilter component
    Deleted the ProductFilter component and its import references. This change cleans up unused code and simplifies the component structure for the project.

--- src/components/ProductFilters/index.tsx
+++
@@ -1,1 +0,0 @@
-export {default as ProductFilter} from './ProductFilter'
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,6 +5,4 @@ export {SearchForm} from './SearchForm';
 export {BrandLogo} from './BrandLogo';
 export {NavigationMenu} from './NavbarMenu';
 export {SocialLinks} from './SocialLinks';
-export {ProductFilter} from './ProductFilters'
-export {Loading} from './Loading'
-// export { ProductsNoContent } from "./Products/products-no-content";
+export {Loading} from './Loading';
