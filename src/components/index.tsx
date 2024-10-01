2d40fcbc2a2d4d091ab40d3f5640fe967dc34e34
garikhg <garik.28h@gmail.com>
2024-10-02 00:04:55 +0400

    Remove NavigationMenu export from components
    The NavigationMenu export was redundant and removed from the index file. This change helps to simplify component exports and reduce unused code.

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,4 @@ export {SEOHead} from './SEOHead';
 export {Footer} from './Footer';
 export {SearchForm} from './SearchForm';
 export {BrandLogo} from './BrandLogo';
-export {NavigationMenu} from './NavbarMenu';
 export {SocialLinks} from './SocialLinks';
