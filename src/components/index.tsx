14f8b6f6e3f2a116086344526c8d579b866f5424
garikhg <garik.28h@gmail.com>
2024-10-05 16:36:01 +0400

    Renamed a footer component replace name to lowercase

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,6 @@
 export {Header} from './header';
 export {SEOHead} from './SEOHead';
-export {Footer} from './Footer';
+export {Footer} from './footer';
 export {SearchForm} from './SearchForm';
 export {BrandLogo} from './BrandLogo';
 export {SocialLinks} from './SocialLinks';
