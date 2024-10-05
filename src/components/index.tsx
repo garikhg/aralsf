4151f44d0ae3d19c9137fa0c46a97c852af3a180
garikhg <garik.28h@gmail.com>
2024-10-06 01:00:04 +0400

    Delete SEOHead Component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,4 @@
 export {Header} from './header';
-export {SEOHead} from './SEOHead';
 export {Footer} from './footer';
 export {SearchForm} from './SearchForm';
 export {BrandLogo} from './BrandLogo';
