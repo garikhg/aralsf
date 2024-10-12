8cc87d573bdb38e5aa541386a31b0c15eb18f0ab
garikhg <garik.28h@gmail.com>
2024-10-13 00:42:40 +0400

    Update swr-provider

--- src/app/providers/swr-provider.tsx
+++ src/app/providers/swr-provider.tsx
@@ -1,7 +1,16 @@
 'use client';
 import {SWRConfig} from 'swr';
 import React from "react";
+import {fetcher} from "@/lib/fetcher";
 
 export const SWRProvider = ({children}: { children: React.ReactNode }) => {
-    return <SWRConfig>{children}</SWRConfig>
+    return <SWRConfig value={{
+        fetcher: fetcher,
+        onError: (error) => {
+            console.error( 'SWR Error:', error )
+        }
+    }}
+    >
+        {children}
+    </SWRConfig>
 };
