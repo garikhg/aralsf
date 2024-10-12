84b20568a36c9b615fd98b3ba0ef1cecf91f326f
garikhg <garik.28h@gmail.com>
2024-10-12 23:52:39 +0400

    Minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import {Cover, HeroSlider, PartnersCarousel} from '@/_components';
 import {BlockMediaText} from '@/components/blocks/MediaText';
 
 export default function Home() {
-
     return (
         <main role="main">
             <HeroSlider/>
