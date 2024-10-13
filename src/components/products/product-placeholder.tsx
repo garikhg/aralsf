92d9a91677e4e261cce1c0e2349a58f61af883d4
garikhg <garik.28h@gmail.com>
2024-10-14 01:50:21 +0400

    Changed a product placeholder image component fix a height change background to gray-50 change color to gray-100 and change icon height auto to full

--- src/components/products/product-placeholder.tsx
+++ src/components/products/product-placeholder.tsx
@@ -3,8 +3,8 @@ import { Image } from 'lucide-react';
 
 const ProductPlaceholderImage = () => {
     return (
-        <div className="flex items-center bg-gray-100 text-gray-200 h-80 w-full rounded-xl overflow-hidden">
-            <Image strokeWidth={1.3} className="w-full h-auto" />
+        <div className="min-h-[435px] flex items-center bg-gray-50 text-gray-100 w-full rounded-xl overflow-hidden">
+            <Image strokeWidth={1.1} className="w-full h-full" />
         </div>
     );
 };
