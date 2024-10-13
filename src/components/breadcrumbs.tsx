06e6a14fd6692695fc50fb047ccc008080664097
garikhg <garik.28h@gmail.com>
2024-10-13 22:13:38 +0400

    Update breadcrumbs created dynamic

--- src/components/breadcrumbs.tsx
+++ src/components/breadcrumbs.tsx
@@ -1,31 +1,71 @@
+'use client';
+
 import React from 'react';
 import {
     Breadcrumb,
     BreadcrumbItem,
     BreadcrumbLink,
-    BreadcrumbList, BreadcrumbPage,
+    BreadcrumbList,
+    BreadcrumbPage,
     BreadcrumbSeparator
 } from "@/components/ui/breadcrumb";
 import {Slash} from "lucide-react";
-import {Container} from "@/components/container";
+import {Skeleton} from "@/components/ui/skeleton";
+import {usePathname} from "next/navigation";
 
-const Breadcrumbs = ({title}: { title?: string }) => {
-    if (!title) return false;
+const Breadcrumbs = ({loading = true}: { loading?: boolean }) => {
+    const pathname = usePathname();
+    const paths = pathname.split( '/' ).filter( Boolean );
 
     return (
-        <Container className="hidden sm:block py-6 pb-0 md:py-6 md:pb-0 xl:py-6 xl:pb-0">
-            <Breadcrumb>
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator><Slash/></BreadcrumbSeparator>
-                    <BreadcrumbItem>
-                        <BreadcrumbPage>{title}</BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
-        </Container>
+        <div className="h-5 min-w-64 hidden sm:flex justify-start items-center mb-8 xl:mb-16">
+            <div className="w-fit">
+                {loading ? (
+                    <Skeleton className="h-3 w-full min-w-64 bg-gray-200 rounded-full"/>
+                ) : (
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem>
+                                <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                            </BreadcrumbItem>
+                            {paths && paths.map( (path: string, index: number) => {
+                                const isListItem = index === paths.length - 1;
+                                const pageURL = `/${paths.slice( 0, index + 1 ).join( '/' )}`;
+                                const pageName = `${path.charAt( 0 ).toUpperCase()}${path.slice( 1 )}`;
+                                const pageTitle = pageName.replace( '-', ' ' );
+
+                                return (
+                                    <React.Fragment key={index}>
+                                        {isListItem ? (
+                                            <>
+                                                <BreadcrumbSeparator>
+                                                    <Slash/>
+                                                </BreadcrumbSeparator>
+                                                <BreadcrumbItem>
+                                                    <BreadcrumbPage>{pageTitle}</BreadcrumbPage>
+                                                </BreadcrumbItem>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <BreadcrumbSeparator>
+                                                    <Slash/>
+                                                </BreadcrumbSeparator>
+                                                <BreadcrumbItem>
+                                                    <BreadcrumbLink href={pageURL}>
+                                                        <span>{pageTitle}</span>
+                                                    </BreadcrumbLink>
+                                                </BreadcrumbItem>
+                                            </>
+                                        )}
+
+                                    </React.Fragment>
+                                )
+                            } )}
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                )}
+            </div>
+        </div>
     );
 };
 
