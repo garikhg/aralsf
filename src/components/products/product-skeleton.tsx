ab077952c4b24d0aba9ccd2efa3ffad1db5c3a81
garikhg <garik.28h@gmail.com>
2024-10-14 00:18:27 +0400

    Modified `product-skeleton` component added border bottom to image skeleton and changed background to gray-100

--- src/components/products/product-skeleton.tsx
+++ src/components/products/product-skeleton.tsx
@@ -4,12 +4,14 @@ import {Skeleton} from "@/components/ui/skeleton";
 const ProductSkeleton = () => {
     return (
         <div className="bg-white w-full rounded-xl p-4">
-            <Skeleton className="bg-gray-200 h-80 w-full rounded-xl overflow-hidden mb-4"/>
-            <Skeleton className="bg-gray-200 h-5 w-48 rounded-xl overflow-hidden"/>
+            <Skeleton className="bg-gray-100 h-80 w-full rounded-xl overflow-hidden mb-4"/>
+            <hr className="border-b border-gray-100 mb-6" />
+
+            <Skeleton className="bg-gray-100 h-5 w-48 rounded-xl overflow-hidden"/>
             <div className="space-y-2 pt-6">
-                <Skeleton className="bg-gray-200 h-4 w-full rounded-xl overflow-hidden"/>
-                <Skeleton className="bg-gray-200 h-4 w-full rounded-xl overflow-hidden"/>
-                <Skeleton className="bg-gray-200 h-4 w-full rounded-xl overflow-hidden"/>
+                <Skeleton className="bg-gray-100 h-4 w-full rounded-xl overflow-hidden"/>
+                <Skeleton className="bg-gray-100 h-4 w-full rounded-xl overflow-hidden"/>
+                <Skeleton className="bg-gray-100 h-4 w-full rounded-xl overflow-hidden"/>
             </div>
         </div>
     );
