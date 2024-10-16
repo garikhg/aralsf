285f44e2290071545ec7ca09554325e1080b1bcb
garikhg <garik.28h@gmail.com>
2024-10-16 21:32:37 +0400

    Fixed page pagination component

--- src/components/page-pagination.tsx
+++ src/components/page-pagination.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
     Pagination,
-    PaginationContent,
-    PaginationItem,
+    PaginationContent, PaginationEllipsis,
+    PaginationItem, PaginationLink,
     PaginationNext,
     PaginationPrevious
 } from "@/components/ui/pagination";
@@ -21,8 +21,35 @@ const PagePagination: React.FC<PagePaginationProps> = ({currentPage, totalPages,
 
     const getPageNumbers = () => {
         const pageNumbers = [];
+        if (totalPages <= maxVisiblePages) {
+            for (let i = 1; i <= totalPages; i++) {
+                pageNumbers.push( i );
+            }
+        } else {
+            if (currentPage <= 3) {
+                for (let i = 1; i <= 4; i++) {
+                    pageNumbers.push( i );
+                }
+                pageNumbers.push( 'ellipsis' );
+                pageNumbers.push( totalPages );
+            } else if (currentPage >= totalPages - 2) {
+                pageNumbers.push( 1 )
+                pageNumbers.push( 'ellipsis' );
+                for (let i = totalPages - 3; i <= totalPages; i++) {
+                    pageNumbers.push( i );
+                }
+            } else {
+                pageNumbers.push( 1 );
+                pageNumbers.push( 'ellipsis' );
+                for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+                    pageNumbers.push( i );
+                }
+                pageNumbers.push( 'ellipsis' );
+                pageNumbers.push( totalPages );
+            }
+        }
+        return pageNumbers;
     }
-    console.log( isLoading )
     return (
         <Pagination className="mt-8 items-center justify-between">
             <PaginationContent>
@@ -39,6 +66,25 @@ const PagePagination: React.FC<PagePaginationProps> = ({currentPage, totalPages,
                         }}
                     />
                 </PaginationItem>
+                {getPageNumbers().length > 1 && getPageNumbers().map( (pageNuber, index) => (
+                    <PaginationItem key={index}>
+                        {pageNuber === 'ellipsis' ? (
+                            <PaginationEllipsis/>
+                        ) : (
+                            <PaginationLink
+                                href="#"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    onPageChange( pageNuber as number );
+                                }}
+                                className={cn( currentPage === pageNuber ? 'pointer-events-none bg-accent' : '' )}
+                                isActive={currentPage === pageNuber}
+                            >
+                                {pageNuber}
+                            </PaginationLink>
+                        )}
+                    </PaginationItem>
+                ) )}
                 <PaginationItem>
                     <PaginationNext
                         href="#"
