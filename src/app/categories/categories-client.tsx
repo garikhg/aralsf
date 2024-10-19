ad8b7c732b1ee7964854807b2b2d2b7989ada934
garikhg <garik.28h@gmail.com>
2024-10-19 16:44:14 +0400

    Fixed: component CategoriesClient page header hero image background src get to a path from fetching date

--- src/app/categories/categories-client.tsx
+++ src/app/categories/categories-client.tsx
@@ -23,32 +23,38 @@ const CategoriesClient: React.FC = () => {
     // Page
     const title = page?.title?.rendered || 'Categories';
     const description = page?.acf?.description || '';
+    let featuredMedia = page?._embedded?.['wp:featuredmedia'] ? page?._embedded?.['wp:featuredmedia']?.[0] : null;
+    const featuredMediaSRC = featuredMedia ? featuredMedia?.source_url : '';
 
     // Categories
     let categories = data || [];
     categories = categories?.filter( (cat: any) => cat.slug !== 'uncategory' && cat.slug !== 'all-products' );
 
+    console.log( page )
+    console.log( featuredMediaSRC )
+
     return (
         <div className="container py-8 xl:py-16 xl:pt-6 min-h-screen">
-            <div className="relative bg-gray-100 h-[360px] lg:h-[480px] rounded-lg overflow-hidden mb-6">
+            {featuredMediaSRC ? (
+                <div className="relative bg-gray-100 h-[360px] lg:h-[480px] rounded-lg overflow-hidden mb-6">
                 <span
                     className="block w-full h-full bg-no-repeat bg-center bg-cover"
-                    style={{backgroundImage: 'url("http://aralsf.local/wp-content/uploads/2024/10/category-h1_1920x1080.jpg")'}}
-                ></span>
-                <div
-                    className="w-full h-full absolute flex items-center top-0 left-0 bg-gradient-to-t bg-black/40 to-transparent text-primary-foreground z-20">
-                    <div className="w-full max-w-screen-md text-center mx-auto p-8">
-                        <Heading1 className="font-medium mb-2">
-                            <span dangerouslySetInnerHTML={{__html: title}}/>
-                        </Heading1>
+                    style={{backgroundImage: `url('${featuredMediaSRC}')`}}
+                />
+                    <div
+                        className="w-full h-full absolute flex items-center top-0 left-0 bg-gradient-to-t bg-black/40 to-transparent text-primary-foreground z-20">
+                        <div className="w-full max-w-screen-md text-center mx-auto p-8">
+                            <Heading1 className="font-medium mb-2">
+                                <span dangerouslySetInnerHTML={{__html: title}}/>
+                            </Heading1>
 
-                        {description ? (
-                            <div className="text-base py-3" dangerouslySetInnerHTML={{__html: description}}/>
-                        ) : ('')}
+                            {description ? (
+                                <div className="text-base py-3" dangerouslySetInnerHTML={{__html: description}}/>
+                            ) : ('')}
+                        </div>
                     </div>
                 </div>
-            </div>
-
+            ) : ('')}
             <PageBreadcrumb loading={false}/>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {categories && categories.map( (category: any) => (
